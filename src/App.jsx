0f865652e50155b0de680f74caa8aa804fffbabe
jrazad10 <jrazad10@gmail.com>
2024-03-14 14:25:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-semibold bg-green-300 rounded-2xl'>KNOWLEDGE IS POWER</h1>
+      
+    </>
+  )
+}
+
+export default App
